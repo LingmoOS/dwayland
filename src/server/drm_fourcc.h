ef28807d050df4b2c7e5c01366ce7b0553453b1f
Lingmo <lingmo@lingmo.org>
2026-04-28 01:19:28 +0800

    Initial commit

---
+++ src/server/drm_fourcc.h
@@ -0,0 +1,13 @@
+/*
+    SPDX-FileCopyrightText: 2011 Intel Corporation
+
+    SPDX-License-Identifier: MIT
+*/
+
+#pragma once
+
+/*
+ * Use the system DRM fourcc definitions to avoid conflicting with the
+ * kernel-provided integer types on Debian 13.
+ */
+#include <drm/drm_fourcc.h>
